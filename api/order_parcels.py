f941273122f9b9260aa4b324bef049551c2b9618
dorotheeasiimwe <dorotheeasiimwe@gmail.com>
2018-11-12 14:25:51 +0300

    add get endpoint

--- api/order_parcels.py
+++ api/order_parcels.py
@@ -6,30 +6,28 @@ class Order:
         self.orders= []
 
     def get_orders(self):
-        return self.orders
+        return {'All parcels': self.orders}
             
-    def create_parcel_orders(self, user_name, parcel_type, pick_up, destination, price, status,parcel_id):
+    def create_parcel_orders(self, user_name, parcel_type, pick_up, destination, weight, status,parcel_Id):
  
         parcels = [order for order in self.orders]
-        parcel_id = len(parcels) + 1
+        parcel_Id = len(parcels) + 1
 
         order = {
             'user_name': user_name, 
             'parcel_type': parcel_type,
             'pick_up': pick_up, 
             'destination': destination,
-            'price': price,
-            'status':status,
-            'parcel_id': parcel_id
+            'weight': weight,
+            'status': status,
+            'parcel_Id': parcel_Id
         }
         self.orders.append(order)
         return self.orders
 
     def get_one_order(self, parcel_Id):
-
-        """ get specific order"""
-
-        one_order = [order for order in self.orders if order['parcel_id'] == parcel_Id]
-        if not one_order['parcel_id']:
-            return {'parcel_id':"Parcel Id doesn't exist"}
-        return {'Requested order': one_order}
+        parcel_id_now = [order for order in self.orders 
+                               if order ['parcel_Id'] == parcel_Id]
+        if not parcel_id_now:
+            return {parcel_Id:"Parcel_id doesnot exist"}
+        return {'Requested order': parcel_id_now}        
--- api/routes.py
+++ api/routes.py
@@ -5,7 +5,10 @@ class Routes:
     def get_urls(order):
        
         orders_view = OrderMenu.as_view('parcels')
-        order.add_url_rule('/api/v1/parcels', view_func=orders_view, methods=['GET'])
+        order.add_url_rule('/api/v1/parcels', view_func=orders_view,
+                           defaults={'parcel_Id': None}, methods=['GET',])
+        order.add_url_rule('/api/v1/parcels/<int:parcel_Id>',
+                           view_func=orders_view, methods=['GET',])
         order_post = OrderMenu.as_view('post_parcels')
         order.add_url_rule('/api/v1/parcels', view_func=order_post, methods=['POST'])
                         
