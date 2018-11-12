fcbfd2d2ecc588f5c81229eabda0c3485042b8f6
dorotheeasiimwe <dorotheeasiimwe@gmail.com>
2018-11-12 14:25:00 +0300

    add get method

--- api/main_views.py
+++ api/main_views.py
@@ -8,11 +8,12 @@ order = Order()
 
 class OrderMenu(MethodView):
 
-    def get(self):
-       
-        if order.get_orders() == []:
-            return jsonify ({"order":"No orders at the moment, please make order"})
-        return jsonify({"orders": order.get_orders()})
+    def get(self, parcel_Id):
+        if parcel_Id is None:
+            if order.get_orders() == []:
+                return jsonify ({"order":"No orders"}),404
+            return jsonify(order.get_orders()),200
+        return jsonify (order.get_one_order(parcel_Id)), 200
 
     def post(self):
 
@@ -22,14 +23,15 @@ class OrderMenu(MethodView):
         parcel_type = data.get('parcel_type')
         pick_up = data.get('pick_up')
         destination = data.get('destination')
-        price = data.get('price')
+        weight = data.get('weight')
         status ='pending'
         parcel_id = 'parcel_id'
         
-        order.create_parcel_orders(user_name, parcel_type, pick_up, destination, price, status,parcel_id)
+        order.create_parcel_orders(user_name, parcel_type, pick_up, destination, weight, status, parcel_id)
         response_object = order.__dict__
         return jsonify(response_object) , 201
 
+    
 
      
         
